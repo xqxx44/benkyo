2d4be223c353a20e730b4791f22fe31f628cd3b6
m_sekine <m_sekeine@fujiball.co.jp>
2018-03-01 01:15:01 +0900

    アニメーション練習

--- nodejs_2/app.js
+++ nodejs_2/app.js
@@ -1,6 +1,20 @@
 'use strict';
 
-import $ from "jquery";
+import $ from 'jquery';
+import anime from 'animejs';
 
-const str = $('h1').text();
-$('h1').css({'color': 'red'});
+
+const robot = $('robot');
+$('button').on('click', function() {
+  anime({
+    targets: '#robot',
+    translateX: {
+      value: [100],
+      duration: 150000
+    },
+    //duration: 10000,
+  });
+  console.log("test");
+});
+
+
